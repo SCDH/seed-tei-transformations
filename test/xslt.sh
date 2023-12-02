2ed1c4f5257f46e2c497668f00ae6d99d5c0df55
Christian Lück <christian.lueck@uni-muenster.de>
2023-12-02 01:21:57 +0100

    added Xerces as dependency in order to get base URI fixup
    This is related to #11.

--- test/xslt.sh
+++ test/xslt.sh
@@ -2,5 +2,6 @@
 
 JARS=${project.build.directory}/lib/Saxon-HE-${saxon.version}.jar
 JARS=$JARS:${project.build.directory}/lib/xmlresolver-${xmlresolver.version}.jar
+JARS=$JARS:${project.build.directory}/lib/xercesImpl-${xerces.version}.jar
 
-java -Ddebug="true" -cp $JARS net.sf.saxon.Transform $@
+java -Ddebug="true" -Dhttp://apache.org/xml/features/xinclude/fixup-base-uris=true -cp $JARS net.sf.saxon.Transform $@
