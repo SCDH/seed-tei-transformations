0fe247d75e22928cfc0b5be61f2a0e9bb9be6ed6
Christian Lück <christian.lueck@uni-muenster.de>
2024-03-21 08:41:14 +0100

    make saxon process XIncludes, log XSLT messages to file
    This closes /SCDH/hees-alea/edition-ibn-nubatah#27.

--- test/alea-prose-tex.sh
+++ test/alea-prose-tex.sh
@@ -4,6 +4,8 @@ INPUT=$1
 
 OUTPUT=/tmp/test.tex
 
+XSLT_LOG=$OUTPUT.transform.log
+
 SEED_DIR=$(dirname $(realpath $0))/..
 
 STYLESHEET=$SEED_DIR/xsl/projects/alea/latex/prose.xsl
@@ -17,7 +19,7 @@ XSLT_CMD=$SEED_DIR/target/bin/xslt.sh
 #echo "$XSLT_CMD -config:$SEED_DIR/saxon.he.xml -xsl:$STYLESHEET -s:$INPUT $PARAMS -o:$OUTPUT && latexmk -lualatex $OUTPUT"
 set -x
 
-$XSLT_CMD -config:$SEED_DIR/saxon.he.xml -xsl:$STYLESHEET -s:$INPUT $PARAMS $FONTPARAMS -o:$OUTPUT && cd $(dirname $OUTPUT) && latexmk -lualatex $OUTPUT
+$XSLT_CMD -config:$SEED_DIR/saxon.he.xml -xsl:$STYLESHEET -xi -s:$INPUT $PARAMS $FONTPARAMS -o:$OUTPUT  2> $XSLT_LOG && cd $(dirname $OUTPUT) && latexmk -lualatex $OUTPUT
 
 cd $OLDPWD
 
